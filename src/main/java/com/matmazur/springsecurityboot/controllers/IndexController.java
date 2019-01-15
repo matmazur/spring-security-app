83ee48dfea88f7c6a665903ca34fc442292116a3
Bingo Bobo <matmazur90@gmail.com>
2019-01-15 22:09:53 +0100

    cleared project for further work

--- src/main/java/com/matmazur/springsecurityboot/controllers/IndexController.java
+++ src/main/java/com/matmazur/springsecurityboot/controllers/IndexController.java
@@ -9,18 +9,8 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(ModelMap modelMap) {
-
         modelMap.put("message", "Hello Secure World!");
         return "index";
     }
 
-    @GetMapping("/secret")
-    public String secret() {
-        return "secret";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
 }
