01b633e2ab8a8ed56a75f144d723dea74812b70d
Bingo Bobo <matmazur90@gmail.com>
2019-01-15 23:15:13 +0100

    added 2 sql schemas to the project, one creating 2 tables for users and roles, second for creating users and roles with further assigning roles to the users. commented other option of user creation in a security configuration code

--- src/main/java/com/matmazur/springsecurityboot/configuration/SecurityConfig.java
+++ src/main/java/com/matmazur/springsecurityboot/configuration/SecurityConfig.java
@@ -1,20 +1,36 @@
 package com.matmazur.springsecurityboot.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
+//                .withUser("mike").password("{noop}ekim").roles("USER")
+//                .and()
+//                .withUser("user").password("root").roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll();
+                .formLogin();
     }
 }
