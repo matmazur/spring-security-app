df24249670b3e941028abf9ccfb9aa135efda4ed
Bingo Bobo <matmazur90@gmail.com>
2019-01-15 13:38:15 +0100

    inital commit

---
+++ src/test/java/com/matmazur/springsecurityboot/SpringSecurityBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.springsecurityboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
